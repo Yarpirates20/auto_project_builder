8456f6e9107e5fce8409ecffab1d36828f5c3fa2
Robert Samoraj <rsamoraj11@gmail.com>
2023-07-14 11:39:50 -0500

    Create functions for user input of project name and author

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// PROTOTYPES
+string getProjectName();
+string getAuthorName();
 
 int main()
 {
+	string projectName = getProjectName();
+	string author = getAuthorName();
 
 
 	return 0;
+}
+
+string getProjectName()
+{
+	string projectName;
+	cout << "What is the name of your project?\n";
+	getline(cin, projectName);
+	return projectName;
+}
+
+string getAuthorName()
+{
+	string author;
+	cout << "\nWho is the author of this project?\n";
+	getline(cin, author);
+	return author;
 }
