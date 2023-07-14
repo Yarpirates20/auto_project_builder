35eb250cc6b6a4d199b3844f11047fe1246c1567
Robert Samoraj <rsamoraj11@gmail.com>
2023-07-14 12:05:28 -0500

    Add bool function to validate project name

--- main.cpp
+++ main.cpp
@@ -5,9 +5,14 @@ using namespace std;
 // PROTOTYPES
 string getProjectName();
 string getAuthorName();
+bool validateProjectName(string);
+bool validateAuthorName(string);
 
 int main()
 {
+	// TODO: use c-string array with size 40 for project name
+	// Validate by checking if each char isnum, isalpha, or is '-'
+	// Change function type to const char *
 	string projectName = getProjectName();
 	string author = getAuthorName();
 
@@ -15,7 +20,14 @@ int main()
 		<< "Project name: " << projectName << '\n'
 		<< "Author:       " << author << '\n';
 
-
+	if (validateProjectName(projectName) == false)
+	{
+		cout << "\nERROR: Invalid project name!\n";
+	}
+	else
+	{
+		cout << "\nPROJECT NAME VALID!\n";
+	}
 	return 0;
 }
 
@@ -33,4 +45,22 @@ string getAuthorName()
 	cout << "\nWho is the author of this project?\n";
 	getline(cin, author);
 	return author;
+}
+
+bool validateProjectName(string pname)
+{
+	// TODO: Validate that project name contains only letters, dashes, and numbers, and is bettwen 1-40 char in length
+	if (pname.length() < 1 || pname.length() > 40)
+	{
+		return false;
+	}
+	else if (!isalnum)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+	
 }
